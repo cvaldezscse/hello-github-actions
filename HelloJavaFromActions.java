af98c8077c40bf148bba32150862ae7d511cd47e
Carlos Valdez <cvaldez.scse@gmail.com>
2025-06-18 16:41:21 -0600

    Create HelloJavaFromActions.java

---
+++ HelloJavaFromActions.java
@@ -0,0 +1,7 @@
+public class HelloJavaFromActions {
+  
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo! Esta es la prueba que tenemos para la parte de correr un workflow de github actions");
+    }
+  
+}
